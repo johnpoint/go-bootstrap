f41954b9cf27f450a7e2a1a23ca26441e4043d2f
johnpoint <me@lvcshu.com>
2023-05-23 14:08:44 +0800

    fix: 添加返回响应函数

--- gin/epcodec.go
+++ gin/epcodec.go
@@ -1,7 +1,23 @@
 package gin
 
+import "net/http"
+
 type JsonCodecEp struct{}
 
 func (d JsonCodecEp) Codec() Codec {
 	return JSONCodec{}
+}
+
+func (d JsonCodecEp) httpResponseError(w http.ResponseWriter, code int, err error) {
+	if code < 200 || code > 599 {
+		code = http.StatusInternalServerError
+	}
+
+	d.httpResponse(w, code, err)
+}
+
+func (d JsonCodecEp) httpResponse(w http.ResponseWriter, code int, v any) {
+	w.Header().Add("Content-Type", d.Codec().Mime())
+	w.WriteHeader(code)
+	d.Codec().NewEncoder(w).Encode(v)
 }
