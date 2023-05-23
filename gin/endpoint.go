4046044b7285a3f480d1cbf641b6b1412b585a0f
johnpoint <me@lvcshu.com>
2023-05-23 14:50:19 +0800

    feat: 添加修改中间件函数

--- gin/endpoint.go
+++ gin/endpoint.go
@@ -8,6 +8,7 @@ type Ep interface {
 	Path() string
 	HandlerFunc() gin.HandlerFunc
 	Middleware() []gin.HandlerFunc
+	SetMiddleware()
 	Codec() Codec
 	HttpResponseError(w http.ResponseWriter, code int, err error)
 	HttpResponse(w http.ResponseWriter, code int, v any)
