45633737c1ce8822327f196daace32dc62a59f93
johnpoint <me@lvcshu.com>
2023-05-23 14:39:21 +0800

    fix: 修复实现接口

--- gin/epcodec.go
+++ gin/epcodec.go
@@ -8,15 +8,15 @@ func (d JsonCodecEp) Codec() Codec {
 	return JSONCodec{}
 }
 
-func (d JsonCodecEp) httpResponseError(w http.ResponseWriter, code int, err error) {
+func (d JsonCodecEp) HttpResponseError(w http.ResponseWriter, code int, err error) {
 	if code < 200 || code > 599 {
 		code = http.StatusInternalServerError
 	}
 
-	d.httpResponse(w, code, err)
+	d.HttpResponse(w, code, err)
 }
 
-func (d JsonCodecEp) httpResponse(w http.ResponseWriter, code int, v any) {
+func (d JsonCodecEp) HttpResponse(w http.ResponseWriter, code int, v any) {
 	w.Header().Add("Content-Type", d.Codec().Mime())
 	w.WriteHeader(code)
 	d.Codec().NewEncoder(w).Encode(v)
